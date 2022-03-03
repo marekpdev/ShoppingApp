8be03e93695fdb638278a72ed795ec2747c3a8eb
Marek Pszczolka <feorin94@gmail.com>
2022-03-03 21:51:02 +0100

    Update

--- app/src/main/java/com/marekpdev/shoppingapp/ui/checkout/basket/CheckoutProductAdapterDelegate.kt
+++ app/src/main/java/com/marekpdev/shoppingapp/ui/checkout/basket/CheckoutProductAdapterDelegate.kt
@@ -1,7 +1,11 @@
 package com.marekpdev.shoppingapp.ui.checkout.basket
 
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.databinding.DataBindingUtil
 import com.marekpdev.shoppingapp.R
 import com.marekpdev.shoppingapp.databinding.VhCheckoutProductBinding
@@ -29,6 +33,45 @@ class CheckoutProductAdapterDelegate(private val onProductClicked: (Product) -> 
             tvProductPrice.text = item.currency + "" + item.price
 
             Picasso.get().load(item.images.first()).into(ivProductImage)
+
+            val sizesSpinnerAdapter = ArrayAdapter(holder.context, R.layout.support_simple_spinner_dropdown_item, item.availableSizes.map { it.name })
+            spProductSize.adapter = sizesSpinnerAdapter
+
+            spProductSize.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    val color = item.availableSizes[position]
+                    Log.d("FEO33", "Selected $color")
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    Log.d("FEO33", "onNothingSelected")
+                }
+            }
+
+            val colorsSpinnerAdapter = ArrayAdapter(holder.context, R.layout.support_simple_spinner_dropdown_item, item.availableColors.map { it.name })
+            spProductColor.adapter = colorsSpinnerAdapter
+
+            spProductColor.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    val size = item.availableSizes[position]
+                    Log.d("FEO33", "Selected $size")
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    Log.d("FEO33", "onNothingSelected")
+                }
+            }
+
         }
     }
 
