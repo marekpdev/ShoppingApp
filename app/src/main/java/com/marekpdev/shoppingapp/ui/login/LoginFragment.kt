da035b72baad68eb9326ea6b281e55416bf42fa1
Marek Pszczolka <feorin94@gmail.com>
2022-05-07 16:02:22 +0200

    Update

--- app/src/main/java/com/marekpdev/shoppingapp/ui/login/LoginFragment.kt
+++ app/src/main/java/com/marekpdev/shoppingapp/ui/login/LoginFragment.kt
@@ -1,28 +1,60 @@
 package com.marekpdev.shoppingapp.ui.login
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.marekpdev.shoppingapp.R
+import com.marekpdev.shoppingapp.databinding.FragmentAddressesBinding
+import com.marekpdev.shoppingapp.databinding.FragmentLoginBinding
 
 /**
  * Created by Marek Pszczolka on 14/04/2021.
  */
 class LoginFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_login, container, false)
+    private lateinit var binding: FragmentLoginBinding
 
-//        view.findViewById<Button>(R.id.signup_btn).setOnClickListener {
-//            findNavController().navigate(R.id.action_register_to_registered)
-//        }
-        return view
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            lifecycleOwner = this@LoginFragment
+//            productViewModel = viewModel
+//            btnLogin.setOnClickListener {
+//                findNavController().navigate(R.id.action_accountFragment_to_loginFragment)
+//            }
+//
+//            btnRegistration.setOnClickListener {
+//                findNavController().navigate(R.id.action_accountFragment_to_registrationFragment)
+//            }
+            initLayout(this)
+        }
+    }
+
+    private fun initLayout(binding: FragmentLoginBinding) = binding.apply {
+        Log.d("FEO33", "initLayout")
+        btnLogin.setOnClickListener {
+
+        }
+        btnRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registrationFragment)
+        }
+
     }
 
 }
