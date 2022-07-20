cc321bdb3d48c4acea96c4308f86e9b12c0a11af
Marek Pszczolka <feorin94@gmail.com>
2022-07-20 22:18:05 +0200

    Fix issue with showing the same content in home screen

--- app/src/main/java/com/marekpdev/shoppingapp/ui/home/products/HomeProductsAdapterDelegate.kt
+++ app/src/main/java/com/marekpdev/shoppingapp/ui/home/products/HomeProductsAdapterDelegate.kt
@@ -19,18 +19,24 @@ import com.marekpdev.shoppingapp.ui.productvh.ProductHeightConstAdapterDelegate
 /**
  * Created by Marek Pszczolka on 01/03/2022.
  */
-class HomeProductsAdapterDelegate(onProductClicked: (Product) -> Unit,
-                                  onToggleFavourite: (Product) -> Unit) :
+class HomeProductsAdapterDelegate(private val onProductClicked: (Product) -> Unit,
+                                  private val onToggleFavourite: (Product) -> Unit) :
     BaseAdapterDelegate<HomeProducts, BaseViewHolder<VhHomeProductsBinding>>(HomeProducts::class.java){
-
-    private val adapter = BaseAdapter(
-        delegatesManager = AdapterDelegatesManager()
-            .addDelegate(ProductHeightConstAdapterDelegate(onProductClicked, onToggleFavourite))
-//            .addDelegate(HomeShowMoreAdapterDelegate(onShowMoreClicked)) // TODO not needed?
-    )
 
     override fun bindViewHolder(item: HomeProducts, holder: BaseViewHolder<VhHomeProductsBinding>) {
         holder.bind {
+
+            // Instead of creating adapter as a member variable and reusing it in bindViewHolder()
+            // we need to create new instance every time we call bindViewHolder().
+            // The problem is that if we have it as a member variable then
+            // when we call 'adapter.replaceData(item.products)' then the same 'item.products' data
+            // is shown in every ViewHolder that is being served by this HomeProductsAdapterDelegate
+            // So basically when we scroll RecyclerView then every time we bind new data, the same
+            // content (the one that has been most recently bound) is being shown in all ViewHolders
+            val adapter = BaseAdapter(
+                delegatesManager = AdapterDelegatesManager()
+                    .addDelegate(ProductHeightConstAdapterDelegate(onProductClicked, onToggleFavourite))
+            )
 
             rvHomeProducts.layoutManager = LinearLayoutManager(holder.context, LinearLayoutManager.HORIZONTAL, false)
             rvHomeProducts.adapter = adapter
