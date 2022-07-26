91884069d59ce8f266d7daed3139016a2e218b1b
Marek Pszczolka <feorin94@gmail.com>
2022-07-26 23:02:17 +0200

    Add workflow for order time group

---
+++ app/src/main/java/com/marekpdev/shoppingapp/ui/orders/OrderGroup.kt
@@ -0,0 +1,29 @@
+package com.marekpdev.shoppingapp.ui.orders
+
+import org.joda.time.DateTime
+
+/**
+ * Created by Marek Pszczolka on 26/07/2022.
+ */
+enum class OrderGroup(val label: String) {
+    TODAY("Today"),
+    YESTERDAY("Yesterday"),
+    THIS_WEEK("This week"),
+    LAST_WEEK("Last week"),
+    LAST_MONTH("Last month"),
+    LATER("Later");
+
+    fun getOrderGroup(test: DateTime, now: DateTime): OrderGroup{
+        if(test.year < now.year) return LATER
+
+        return when {
+            test.dayOfYear == now.dayOfYear -> TODAY
+            test.dayOfYear == now.dayOfYear - 1 -> YESTERDAY
+            test.weekOfWeekyear == now.weekOfWeekyear -> THIS_WEEK
+            test.weekOfWeekyear == now.weekOfWeekyear - 1 -> LAST_WEEK
+            test.monthOfYear == now.monthOfYear -1 -> LAST_MONTH
+            else -> LATER
+        }
+    }
+
+}
